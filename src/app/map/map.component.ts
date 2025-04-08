891028e7963df3842b8aeced1ec1c555bb36fe1e
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2025-04-08 12:29:14 +0100

    Refactor MapComponent to use MapLibre GL instead of Leaflet and simplify initialization

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,43 +1,18 @@
-import {
-  Component,
-  ElementRef,
-  ViewChild,
-  afterNextRender,
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import L from 'leaflet';
+import { Component, OnInit } from '@angular/core';
+import maplibregl from 'maplibre-gl';
 
 @Component({
   selector: 'app-map',
-  imports: [CommonModule],
-  template: `<div #map class="w-[1200px] h-[600px]"></div> `,
-  styleUrl: './map.component.scss',
+  template: `<div id="map" class="w-full h-full"></div>`,
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
-  @ViewChild('map') mapElement!: ElementRef;
-  private map: L.Map | null = null;
-
-  constructor() {
-    afterNextRender(() => {
-      this.initMap();
+export class MapComponent implements OnInit {
+  ngOnInit(): void {
+    const map = new maplibregl.Map({
+      container: 'map',
+      style: 'http://localhost:8080/styles/basic-preview/style.json',
+      center: [-6.0, 32.0],
+      zoom: 2
     });
-  }
-
-  private initMap(): void {
-    // Morocco's center coordinates
-    const moroccoCenter: L.LatLngExpression = [31.6295, -7.9811];
-    
-    // Initialize the map
-    this.map = L.map(this.mapElement.nativeElement).setView(moroccoCenter, 6);
-
-    // Use the local tile server
-    L.tileLayer('http://localhost:8080/tile/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap contributors',
-    }).addTo(this.map);
-
-    // Add a marker for your location
-    const homeMarker = L.marker([30.4278, -9.5981]).addTo(this.map);
-    homeMarker.bindPopup('Kamal home').openPopup();
   }
 }
