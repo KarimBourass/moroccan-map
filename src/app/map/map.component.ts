18a291114b00ef234c31e4fae32fe9a750c7a7df
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2025-04-08 13:16:36 +0100

    Update MapComponent to adjust map center, zoom, and apply filters for disputed borders

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,12 +7,36 @@ import maplibregl from 'maplibre-gl';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  ngOnInit(): void {
+  ngOnInit(): void {  
     const map = new maplibregl.Map({
       container: 'map',
       style: 'http://localhost:8080/styles/basic-preview/style.json',
-      center: [-6.0, 32.0],
-      zoom: 2
+      center: [-8.0, 28.0], 
+      zoom: 5,
+      maxBounds: [
+        [-17.5, 20.5],
+        [-0.9, 36.0]   
+      ]
     });
+
+
+
+    map.on('load', () => {
+      // Apply the filter to hide borders with disputed_name 'WesternSaharaclaimwithMorocco'
+      map.setFilter('admin_sub', ['!=', 'disputed_name', 'WesternSaharaclaimwithMorocco']);
+      map.setFilter('admin_country', ['!=', 'disputed_name', 'WesternSaharaclaimwithMorocco']);
+    });
+
+    // map.on('load', () => {
+    //   // Query features in the border layers (admin_sub and admin_country)
+    //   const features = map.queryRenderedFeatures({ layers: ['admin_sub', 'admin_country'] });
+
+    
+    //   features.forEach(feature => {
+    //     console.log('Feature ID:', feature.id);
+    //     console.log('Feature Properties:', feature.properties);
+    //     console.log('Feature Geometry:', feature.geometry);
+    //   });
+    // });
   }
 }
