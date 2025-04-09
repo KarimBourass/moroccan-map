000ed9dfbb67feb4968979a4c0d0d33cbcd15f27
Abdelmalek123-Ennani <ahmam3600@gmail.com>
2025-04-09 15:57:51 +0100

    Enhance MapComponent to apply hidden states for specific border features and adjust line opacity based on feature state

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -19,13 +19,39 @@ export class MapComponent implements OnInit {
       ]
     });
 
-
-
     map.on('load', () => {
-      // Apply the filter to hide specific borders
-      map.setFilter('admin_sub', ['!=', 'disputed_name', 'WesternSaharaclaimwithMorocco']);
-      map.setFilter('admin_country', ['!=', 'disputed_name', 'WesternSaharaclaimwithMorocco']);
+      const hiddenIds = [888, 887, 886, 885, 311, 312, 313, 314, 315, 
+        316, 317, 318, 646, 647, 648, 649, 248, 249,251, 244, 245, 246, 
+        247, 250, 242, 225, 226, 227, 228,74, 75, 77, 70, 71, 72, 73,
+        76, 68, 69, 44, 16, 17,18, 19, 10, 11, 12, 9, 19,20, 21, 1, 21, 22,
+        25, 18, 17, 8, 9, 6, 7, 2, 4, 5, 6, 14, 15, 16, 13, 12];
+    
+      function applyHiddenStates() {
+        hiddenIds.forEach(id => {
+          map.setFeatureState(
+            {
+              source: 'openmaptiles',
+              sourceLayer: 'boundary',
+              id: id
+            },
+            { hidden: true }
+          );
+        });
+      }
+    
+      // Initial application
+      applyHiddenStates();
+    
+      // Set paint property
+      map.setPaintProperty('admin_country', 'line-opacity', [
+        'case',
+        ['boolean', ['feature-state', 'hidden'], false],
+        0, // Hidden if true
+        1  // Visible if false
+      ]);
+  
     });
+
 
     // map.on('load', () => {
     //   // Query features in the border layers (admin_sub and admin_country)
